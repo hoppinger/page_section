89bf104d68689dab4037c249671e0aaf474076a0
Rolf van de Krol <info@rolfvandekrol.nl>
2014-10-02 23:15:43 +0200

    Comments and whitespace fixes

--- page-section.tpl.php
+++ page-section.tpl.php
@@ -1,3 +1,11 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation to display a page-section.
+ */
+
+?>
 <div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <div class="content"<?php print $content_attributes; ?>>
     <?php
